c31f2b82d8b9b878974cb2a332cc65ff81bcb21e
Kenevian <Katon.vanetten@gmail.com>
2019-11-17 16:41:26 -0500

    Add skeleton thumbnail

---
+++ instagram-display/src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Thumbnail from "./Thumbnail";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+			<Thumbnail/>
+    </div>
+  );
+}
+
+export default App;
---
+++ instagram-display/src/Components/Thumbnail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Thumbnail.css"
+
+export default class Thumbnail extends React.Component {
+	render() {
+		return (
+			<div className="thumbnail-container">
+			</div>
+		);
+	}
+}
