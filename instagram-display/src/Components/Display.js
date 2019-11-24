13591953343b6d2538dfee5bfc42b392b1621557
Kenevian <Katon.vanetten@gmail.com>
2019-11-24 14:24:56 -0500

    Remove Trending and On The Feed headers

--- instagram-display/src/Components/Display.js
+++ instagram-display/src/Components/Display.js
@@ -4,10 +4,22 @@ import React from "react";
 
 import "./Display.css"
 
-let createThumbnail = (article, i) => {
-	return(
-		<Thumbnail key={i} imageurl={article.imageurl} link={article.link}/>
-	);
+const NUM_FEATURED = 2;
+
+let createFeaturedThumbnail = (article, i) => {
+	if(i < NUM_FEATURED){
+		return(
+			<Thumbnail key={i} imageurl={article.imageurl} link={article.link}/>
+		);
+	}
+};
+
+let createGeneralThumbnail = (article, i) => {
+	if(i > NUM_FEATURED){
+		return(
+			<Thumbnail key={i} imageurl={article.imageurl} link={article.link}/>
+		);
+	}
 };
 
 export default class Display extends React.Component{
@@ -16,11 +28,12 @@ export default class Display extends React.Component{
 	}
 
 	render() {
-		var generalArticles = this.props.articles.map(createThumbnail);
+		var featuredArticles = this.props.articles.map(createFeaturedThumbnail);
+		var generalArticles = this.props.articles.map(createGeneralThumbnail);
 		return (
 			<div className="display-container">
 				<Header/>
-				<p className="section-description" >Trending</p>
+				{featuredArticles}
 				{generalArticles}
 			</div>
 		);
