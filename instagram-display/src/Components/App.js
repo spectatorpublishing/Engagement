e1f7ef1c921476b660bcee8d5e95bc0bb76279dc
Carl Dobrovic <cadobrovic@gmail.com>
2019-11-25 14:04:44 -0500

    Initial commit. React fetch() API might not work for this due to nature of Google Sheet endpoint. Need to discuss with Billy and Kevin.

--- instagram-display/src/Components/App.js
+++ instagram-display/src/Components/App.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
+const GoogleSpreadsheet = require('google-spreadsheet');
+const { promisify } = require('util');
+const creds = require('../../sheet_secret.json');
 import Display from "./Display";
 import './App.css';
+
+const sheet_id = '1hUaNrgCxDC5N1hQ528fp-yuxSBCztLU4X6ZR6a1HEbk';
 
 var articlejson = [
 {"link": "https://www.columbiaspectator.com/news/2019/11/11/university-senate-launches-childcare-affordability-initiative-to-support-student-parents/", "imageurl": "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/C6UR3YSF3BBSXBN65VW5TMUVAA.jpg"},
@@ -12,12 +17,39 @@ var articlejson = [
 {"link": "https//www.columbiaspectator.com/news/2019/10/03/qa-inaugural-vp-of-student-affairs-joseph-greenwell-shares-personal-history-talks-most-pressing-campus-issues/", "imageurl": "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/KXJHQTUCTNCRZPI33JCVGOFIRA.jpg"}
 ];
 
-function App() {
-  return (
-    <div className="App">
-			<Display articles = {articlejson}/>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        articles: []
+      };
+    }
+
+    componentDidMount() {
+      // fetch('HTTP GET')
+      // .then(results => {
+      //   console.log(results.json());
+      // })
+      getSheet();
+      
+    }
+
+    async getSheet() {
+      const doc = new GoogleSpreadsheet(sheet_id);
+      await promisify(doc.useServiceAccountAuth)(creds);
+      const info = await promisify(doc.getInfo)();
+      const sheet = info.worksheets[0];
+      console.log(`Title: ${sheet.title}`);
+    }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Display articles = {articlejson}/>
+      </div>
+    );
+  }
 }
 
 export default App;
