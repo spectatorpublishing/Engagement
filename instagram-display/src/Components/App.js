3a2409bdc39e19cbff4d773eddaf8f60ed1c2f2b
Carl Dobrovic <cadobrovic@gmail.com>
2019-12-05 22:09:08 -0500

    Fetch from sheets works with tabletop package.

--- instagram-display/src/Components/App.js
+++ instagram-display/src/Components/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-const GoogleSpreadsheet = require('google-spreadsheet');
-const { promisify } = require('util');
-const creds = require('../../sheet_secret.json');
 import Display from "./Display";
 import './App.css';
+import Tabletop from 'tabletop';
+const config = require('../config');
 
-const sheet_id = '1hUaNrgCxDC5N1hQ528fp-yuxSBCztLU4X6ZR6a1HEbk';
+
+// const sheet_id = '1hUaNrgCxDC5N1hQ528fp-yuxSBCztLU4X6ZR6a1HEbk';
+
+// const API_KEY = '';
+// const CLIENT_ID = '1024054171811-m9856es33vp4ogcsl81tnh4v0khar2oe.apps.googleusercontent.com';
+// var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+// var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
 
 var articlejson = [
 {"link": "https://www.columbiaspectator.com/news/2019/11/11/university-senate-launches-childcare-affordability-initiative-to-support-student-parents/", "imageurl": "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/C6UR3YSF3BBSXBN65VW5TMUVAA.jpg"},
-{"link": "https://www.columbiaspectator.com/news/2019/10/28/morningside-heights-smoke-shops-say-new-york-states-e-cigarette-bill-could-devastate-their-businesses/",	"imageurl": "//arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/FMY3H6ANI5AYBM23GY5VI4QLUY.jpg"},
+{"link": "https://www.columbiaspectator.com/news/2019/10/28/morningside-heights-smoke-shops-say-new-york-states-e-cigarette-bill-could-devastate-their-businesses/",	"imageurl": "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/FMY3H6ANI5AYBM23GY5VI4QLUY.jpg"},
 {"link": "https//www.columbiaspectator.com/sports/2019/10/19/football-defeats-penn-44-6-in-largest-homecoming-victory-in-programs-149-year-history/", "imageurl": "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/JV3NGZQ75ZADBLVIA6PW65WIRU.jpg"},
 {"link": "https://www.columbiaspectator.com/sports/2019/10/14/camacho-ferral-claims-first-place-as-columbia-women-test-younger-runners-at-metropolitan-xc-championships/", "imageurl":"https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/VZQE577BRJA2NEEZSDD2WBEOMM.jpg"},
 {"link": "https://www.columbiaspectator.com/arts-and-entertainment/2019/10/10/more-than-a-market-columbias-greenmarket-sellers-discuss-the-cultural-impact-of-farmers-markets/", "imageurl": "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/Y5ZQFHXBEFG5NACRMFZF2MYZEE.jpg"},
@@ -17,36 +22,42 @@ var articlejson = [
 {"link": "https//www.columbiaspectator.com/news/2019/10/03/qa-inaugural-vp-of-student-affairs-joseph-greenwell-shares-personal-history-talks-most-pressing-campus-issues/", "imageurl": "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/KXJHQTUCTNCRZPI33JCVGOFIRA.jpg"}
 ];
 
+var mykey = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwLERMtTIHzDSF29sZqDsIjkVwOTyIJKMaU-OQsUOOOEX4OSfgPm4tSHhHIvqAxwvBBK_Bdrr-6Dru/pubhtml';
+
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
 class App extends Component {
     constructor() {
       super();
       this.state = {
-        articles: []
-      };
+        data: []
+      }
     }
 
     componentDidMount() {
-      // fetch('HTTP GET')
-      // .then(results => {
-      //   console.log(results.json());
-      // })
-      getSheet();
-      
-    }
+      // 1. Load the JavaScript client library.
 
-    async getSheet() {
-      const doc = new GoogleSpreadsheet(sheet_id);
-      await promisify(doc.useServiceAccountAuth)(creds);
-      const info = await promisify(doc.getInfo)();
-      const sheet = info.worksheets[0];
-      console.log(`Title: ${sheet.title}`);
+      // window.gapi.load("client", this.initClient);
+      Tabletop.init({
+        key: '1hUaNrgCxDC5N1hQ528fp-yuxSBCztLU4X6ZR6a1HEbk',
+        callback: googleData => {
+          console.log('google sheet data --->', googleData)
+          this.setState({
+            data: googleData
+          })
+        },
+        simpleSheet: true
+      })
     }
-
 
   render() {
+    console.log('updated state --->', this.state);
+    const { data } = this.state
+    
     return (
       <div className="App">
-        <Display articles = {articlejson}/>
+        <Display articles = {this.state && this.state.data}/>
+        
       </div>
     );
   }
