733aaf1dabcc8f2d67bef3a28b3350804fe6bf8f
Carl Dobrovic <cadobrovic@gmail.com>
2020-07-13 16:50:16 -0400

    Updates

---
+++ precache-manifest.ad7486d1a2f933870f284418c4a152fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf875dbc89eed193e7d5bed31ee3f6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b86f433365cd6a4fb26e",
+    "url": "/static/css/main.346c3812.chunk.css"
+  },
+  {
+    "revision": "615014bc3351c52be7cb",
+    "url": "/static/js/2.9d1db478.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d1db478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86f433365cd6a4fb26e",
+    "url": "/static/js/main.4c4a87e4.chunk.js"
+  },
+  {
+    "revision": "ee3724866362748f3eb4",
+    "url": "/static/js/runtime-main.4a4647f2.js"
+  }
+]);
