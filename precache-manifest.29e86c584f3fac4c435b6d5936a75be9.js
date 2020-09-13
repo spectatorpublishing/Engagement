e9be0bdf6f8553e737b4822a2dd0bad9e3a810f3
McKenna Gillard <mckennagrace121@gmail.com>
2020-09-13 14:53:44 -0400

    Updates

---
+++ precache-manifest.29e86c584f3fac4c435b6d5936a75be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f60f46237a1ce33662dc14c39abedd",
+    "url": "/spec-insta/index.html"
+  },
+  {
+    "revision": "800dc6c04edd9dd75c72",
+    "url": "/spec-insta/static/css/main.346c3812.chunk.css"
+  },
+  {
+    "revision": "9241eca3ee7cd1df3443",
+    "url": "/spec-insta/static/js/2.6b897a3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spec-insta/static/js/2.6b897a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800dc6c04edd9dd75c72",
+    "url": "/spec-insta/static/js/main.e6587fb6.chunk.js"
+  },
+  {
+    "revision": "8dc489c288ce13050ce4",
+    "url": "/spec-insta/static/js/runtime-main.951b9ffc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Engagement/precache-manifest.9062944fead73f7bf7d272aed31da20d.js"
+  "/spec-insta/precache-manifest.29e86c584f3fac4c435b6d5936a75be9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Engagement/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spec-insta/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
